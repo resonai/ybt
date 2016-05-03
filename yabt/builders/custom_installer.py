59faa3df65abdd3c186d3e6319b1401f8cda8f1f
Itamar Ostricher <itamar@yowza3d.com>
2016-05-03 21:36:30 +0300

    Enhance CustomInstaller builder to support archives and local data in addition to git URI's
    Required significant refactoring of the design of the CustomInstaller builder - It now always creates a single `.tar.gz` file per custom-installer that includes the "package" (whatever the URI / local data points to) + the install script, so there's always just one file per custom installer to transfer into the image for installing.
    
    This has several upsides:
    
    - Get rid of `.git` directory in the image.
    - Since the `.tar.gz` file is extracted & installed & deleted in the same Dockerfile layer - the layer size is kept minimal - at the size of the tarball.
    - A preparation step for a next step in which the tarball isn't even copied into the image, but served by a YABT-spawned server during build-time and deleted within the same Docekrfile layer, so the layer size is minimized to *zero* (or just the actual installed artifacts).
    
    The archive support was needed for protobuf installation, and the local-data support was needed for spark-conf "installation". Git support exists for rpcz installation, but not yet used (since it depends on protobuf and other stuff).

--- yabt/builders/custom_installer.py
+++ yabt/builders/custom_installer.py
@@ -24,50 +24,143 @@ yabt Custom Installer Builder
 """
 
 
+from collections import namedtuple
 import os
-from os.path import basename, join
+from os.path import basename, isfile, join, relpath
+import shutil
+import tarfile
+from urllib.parse import urlparse
 
 import git
 from git import InvalidGitRepositoryError, NoSuchPathError
+import requests
 
 from ..extend import (
     PropType as PT, register_build_func, register_builder_sig,
     register_manipulate_target_hook)
+from ..logging import make_logger
 from ..target_utils import split_name
+
+
+logger = make_logger(__name__)
 
 
 register_builder_sig(
     'CustomInstaller',
     [('uri', PT.str),
      ('script', PT.File),
+     ('local_data', PT.FileList, None),
      ('deps', PT.TargetList, None),
      ('uri_type', PT.str, None),
      ])
 
 
-@register_build_func('CustomInstaller')
-def custom_installer_builder(build_context, target):
-    print('Fetch and cache custom installer package', target)
-    # clone the repository under a private builder workspace
-    workspace_dir = build_context.get_workspace('CustomInstaller', target.name)
+CustomInstaller = namedtuple('CustomInstaller',
+                             ['name', 'package', 'install_script'])
+
+
+def guess_uri_type(uri: str, hint: str=None):
+    # TODO(itamar): do this better
+    if hint:
+        return hint
+    norm_uri = uri.lower()
+    parsed_uri = urlparse(norm_uri)
+    if parsed_uri.path.endswith('.git'):
+        return 'git'
+    if parsed_uri.scheme in ('http', 'https'):
+        return 'archive'
+    return 'local'
+
+
+def gitfilter(tarinfo):
+    if basename(tarinfo.name) in ['.git', '.gitignore']:
+        return None
+    return tarinfo
+
+
+def git_handler(unused_build_context, target, package_dir):
     target_name = split_name(target.name)
-    script_name = basename(target.props.script)
-    package_dir = join(workspace_dir, target_name)
-    install_script = join(workspace_dir, script_name)
+    # clone the repository under a private builder workspace
     try:
         repo = git.Repo(package_dir)
     except (InvalidGitRepositoryError, NoSuchPathError):
         repo = git.Repo.clone_from(target.props.uri, package_dir)
-    # link the install script in the workspace dir
-    try:
-        os.remove(install_script)
-    except:
-        pass
-    os.link(join(build_context.conf.project_root, target.props.script),
-            install_script)
     assert repo.working_tree_dir == package_dir
-    target.props.workspace = workspace_dir
-    target.props.rel_dir_script = (basename(workspace_dir), script_name)
+
+    tar = tarfile.open(target.props.installer_desc.package, 'w:gz',
+                       dereference=True)
+    tar.add(package_dir, arcname=target_name, filter=gitfilter)
+    return tar
+
+
+def archive_handler(unused_build_context, target, package_dir):
+    target_name = split_name(target.name)
+    package_dest = join(package_dir, basename(urlparse(target.props.uri).path))
+    package_content_dir = join(package_dir, 'content')
+    if isfile(package_dest):
+        logger.debug('Archive {} is cached', package_dest)
+    else:
+        logger.debug('Downloading archive {} from {}',
+                     package_dest, target.props.uri)
+        try:
+            shutil.rmtree(package_dir)
+        except FileNotFoundError:
+            pass
+        os.makedirs(package_dir)
+        resp = requests.get(target.props.uri, stream=True)
+        with open(package_dest, 'wb') as archive_file:
+            for chunk in resp.iter_content():
+                archive_file.write(chunk)
+
+    with tarfile.open(package_dest, 'r:*') as tar:
+        tar.extractall(package_content_dir)
+
+    tar = tarfile.open(target.props.installer_desc.package, 'w:gz',
+                       dereference=True)
+    tar.add(package_content_dir, arcname=target_name)
+    return tar
+
+
+def local_handler(build_context, target, package_dir):
+    target_name = split_name(target.name)
+    tar = tarfile.open(target.props.installer_desc.package, 'w:gz',
+                       dereference=True)
+    for local_file in target.props.local_data:
+        tar.add(join(build_context.conf.project_root, local_file),
+                arcname=join(target_name, local_file))
+    return tar
+
+
+@register_build_func('CustomInstaller')
+def custom_installer_builder(build_context, target):
+    # TODO(itamar): Handle cached package invalidation
+    print('Fetch and cache custom installer package', target)
+    workspace_dir = build_context.get_workspace('CustomInstaller', target.name)
+    target_name = split_name(target.name)
+    script_name = basename(target.props.script)
+    package_tarball = '{}.tar.gz'.format(join(workspace_dir, target_name))
+    target.props.installer_desc = CustomInstaller(
+        name=target_name, package=package_tarball, install_script=script_name)
+    if isfile(package_tarball):
+        logger.debug('Custom installer package {} is cached', package_tarball)
+        return
+
+    logger.debug('Making custom installer package {}', package_tarball)
+    handlers = {
+        'git': git_handler,
+        'archive': archive_handler,
+        'local': local_handler,
+    }
+    uri_type = guess_uri_type(target.props.uri, target.props.uri_type)
+    logger.debug('CustomInstaller URI {} typed guessed to be {}',
+                 target.props.uri, uri_type)
+    # TODO(itamar): consider supporting local data in addition to URI
+    tar = handlers[uri_type](build_context, target,
+                             join(workspace_dir, target_name))
+    tar.add(join(build_context.conf.project_root, target.props.script),
+            arcname=join(target_name,
+                         target.props.installer_desc.install_script))
+    tar.close()
 
 
 @register_manipulate_target_hook('CustomInstaller')
