2a28cbe496533653bc9d82c809f8242f18507a73
Shai Ghelberg <bergden@resonai.com>
2018-10-31 13:48:29 +0200

    fix indent bug

--- yabt/buildcontext.py
+++ yabt/buildcontext.py
@@ -464,8 +464,8 @@ class BuildContext:
                 if run_tests and 'testable' in target.tags:
                     if self.conf.no_test_cache or not test_cached:
                         logger.info('Testing target {}', target.name)
-                        retries =
-                        target.props.retries or self.conf.test_retries
+                        retries = (
+                            target.props.retries or self.conf.test_retries)
                         if retries < 0:
                             raise ValueError(
                                 'Retries value must be positive: got {}'
