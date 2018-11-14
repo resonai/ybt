069ea195036e4194ae2a48b4b8b4d90a7b1b62aa
Shai Ghelberg <bergden@resonai.com>
2018-11-14 17:16:33 +0200

    Document test

--- yabt/buildcontext_test.py
+++ yabt/buildcontext_test.py
@@ -28,7 +28,29 @@ import pytest
 from .buildcontext import BuildContext
 from .graph import populate_targets_graph
 
+slow = pytest.mark.skipif(not pytest.config.getoption('--with-slow'),
+                          reason='need --with-slow option to run')
 
-@pytest.mark.usefixtures('??')
-def test_parser_error(basic_conf, capsys):
-  #TODO(bergden) write test with some nice dag, where the failed branch won't run, and successful branch continues
+@slow
+@pytest.mark.usefixtures('in_tests_project')
+def test_continue_after_fail(basic_conf):
+    '''
+    Testing this thing:
+    We want to have targets A, B, C and D where B and D are tests that depand
+    on A and C respectively. A is a faulty build that will cause C to be skipped.
+    D will pass after B is built.
+    To test that we "continued after fail" we need to make sure A runs and fails
+    before we reach B.
+    This is what we want:
+    A -X-> C (skipped)
+    B --> D
+    Such test is icky, because we can't promise anything about the order in which
+    indepedant branches run.
+    If BD would run before AC we can't distinguish that behavior from AC failing
+    and BD running thanks to the flag.
+    Sope, we generate 10 AC-alikes and 10 BD-alikes, and statistically assume at
+    least one BD pair would run after an AD pair.
+    Finally we assert, A failed, C skipped, and BD ran successfuly (10 times).
+    '''
+
+    #TODO(bergden) do it what he said.....
