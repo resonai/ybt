8954931fd45531befd5819b72d137a8952620290
Shai Ghelberg <bergden@resonai.com>
2019-02-19 17:30:24 +0200

    initial work for testing unzipping files while preserving execution permissions

---
+++ tests/tests/hello_custom_installer/custom-installer-test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo 'testing@!!@@@@@@@@@@@@@@@@@@@@@@@@@'
+set -e
+echo $PWD
+echo '-----------------------------'
+ls
+echo '-----------------------------'
+cd custom-installer-test-zip
+python hello_world.py
---
+++ tests/tests/hello_custom_installer/hello_world.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3.4
+# -*- coding: utf-8 -*-
+def main():
+    print('Hello world!')
+
+if u'__main__' == __name__:
+   main()
