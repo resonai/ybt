aaf76ee5472476c9d07e3004be607abfd064ab75
Dana Shamir <dana@resonai.com>
2019-04-10 10:54:54 +0300

    assert that ls are the same

--- yabt/builders/filegroup_test.py
+++ yabt/builders/filegroup_test.py
@@ -1,4 +1,5 @@
 import pytest
+from subprocess import check_output
 
 from yabt.buildcontext import BuildContext
 from yabt.graph import populate_targets_graph
@@ -14,3 +15,9 @@ def test_file_group_dep(basic_conf):
     basic_conf.targets = ['hello:hello-with-data']
     populate_targets_graph(build_context, basic_conf)
     build_context.build_graph(run_tests=True)
+    ls_output_image = str(check_output(
+        ['docker', 'run', '--rm', '--entrypoint', 'ls',
+         build_context.targets['hello:hello-with-data'].image_id,
+         '-R', 'hello/data']))
+    ls_output_here = str(check_output(['ls', '-R', 'hello/data']))
+    assert ls_output_image == ls_output_here
