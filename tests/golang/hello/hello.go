052b4021cc9ef94e80cf57aa0fbd2170612b419c
Eyal Fink <eyal@resonai.com>
2020-06-09 15:03:35 +0300

    fix two bugs in handling env runtime_params

--- tests/golang/hello/hello.go
+++ tests/golang/hello/hello.go
@@ -3,8 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"github.com/common-nighthawk/go-figure"
 )
+func PrintFooFromEnv() string {
+	v := os.Getenv("FOO")
+	return fmt.Sprintf("hello %s", v)
+}
+
 
 func GetGreet(who string) string {
 	return fmt.Sprintf("hello %s", who)
