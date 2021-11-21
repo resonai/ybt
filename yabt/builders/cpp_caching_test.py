0e2a3082869298ab8b935417eca3ec11a49c48c7
dana <dana@resonai.com>
2021-11-21 09:59:58 +0200

    test cpp caching for GTest target too

--- yabt/builders/cpp_caching_test.py
+++ yabt/builders/cpp_caching_test.py
@@ -48,9 +48,18 @@ def build_main_app():
     build_context.build_graph()
 
 
+def build_test():
+    basic_conf = cli.init_and_get_conf(
+        ['--non-interactive', '--continue-after-fail', 'build'])
+    build_context = BuildContext(basic_conf)
+    basic_conf.targets = [':op_user_test']
+    populate_targets_graph(build_context, basic_conf)
+    build_context.build_graph(run_tests=True)
+
+
 @pytest.mark.slow
 @pytest.mark.skip(reason="Not supported feature")
-def test_caching(tmp_dir):
+def test_caching_prog(tmp_dir):
     shutil.copytree(PROJECT_DIR, 'cpp_caching')
     os.chdir('cpp_caching')
 
@@ -66,3 +75,22 @@ def test_caching(tmp_dir):
     build_main_app()
     assert op_obj_timestamp == path.getmtime(OP_OBJ_FILE)  # This assert fails
     assert check_output(['docker', 'run', 'main-app:latest']) == b'20'
+
+
+@pytest.mark.slow
+@pytest.mark.skip(reason="Not supported feature")
+def test_caching_gtest(tmp_dir):
+    shutil.copytree(PROJECT_DIR, 'cpp_caching')
+    os.chdir('cpp_caching')
+
+    build_test()
+    op_obj_timestamp = path.getmtime(OP_OBJ_FILE)
+
+    with open('binary_operation.cc', 'r') as f:
+        binary_operation_code = f.read()
+    with open('binary_operation.cc', 'w') as f:
+        f.write(binary_operation_code.replace('+', '*'))
+
+    with pytest.raises(SystemExit):
+        build_test()
+    assert op_obj_timestamp == path.getmtime(OP_OBJ_FILE)  # This assert fails
