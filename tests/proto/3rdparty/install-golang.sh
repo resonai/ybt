1d2bcc0fd644f6d54473959821d62963074fa697
shir-resonai <shir@resonai.com>
2022-03-17 11:19:06 +0200

    Go get protoc-gen-go-grpc

--- tests/proto/3rdparty/install-golang.sh
+++ tests/proto/3rdparty/install-golang.sh
@@ -13,5 +13,7 @@ mkdir "$GOPATH"
 apt update
 apt-get install -y git
 
-go get github.com/golang/protobuf/protoc-gen-go
+go get google.golang.org/protobuf/cmd/protoc-gen-go
+go get google.golang.org/grpc/cmd/protoc-gen-go-grpc
+
 chmod -R 777 "$GOPATH"
