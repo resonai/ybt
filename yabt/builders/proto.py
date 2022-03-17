090bf18fd9a6afe664babbd19cb912b3b97362e9
shir-resonai <62287748+shir-resonai@users.noreply.github.com>
2022-03-17 18:43:43 +0200

    grpc implies building go (#229)
    * grpc implies building go

--- yabt/builders/proto.py
+++ yabt/builders/proto.py
@@ -89,7 +89,7 @@ def proto_builder(build_context, target):
         protoc_cmd.extend(('--cpp_out', buildenv_workspace))
     if target.props.gen_python:
         protoc_cmd.extend(('--python_out', buildenv_workspace))
-    if target.props.gen_go and not target.props.gen_go_grpc:
+    if target.props.gen_go or target.props.gen_go_grpc:
         protoc_cmd.extend(('--go_out', buildenv_workspace,
                            '--go_opt=paths=source_relative'))
     if target.props.gen_cpp_grpc:
